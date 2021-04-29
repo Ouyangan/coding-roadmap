aefd6e0765385690e318598d151ae416a10dc7da
an.ouyang <an.ouyang@zuopeng.com>
2021-04-29 15:54:47 +0800

    LeetCode-11-盛最多水的容器

---
+++ coding/src/test/java/com/roadmap/algorithm/MaxArea11Test.java
@@ -0,0 +1,15 @@
+package com.roadmap.algorithm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MaxArea11Test {
+
+    @Test
+    void maxArea() {
+        MaxArea11 max = new MaxArea11();
+        int i = max.maxArea(new int[]{4, 3, 2, 1, 4});
+        assertEquals(i, 16);
+    }
+}
