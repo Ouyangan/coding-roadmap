bd61bec8c9b8bafb1bd2303312e9f920087b241e
an.ouyang <an.ouyang@zuopeng.com>
2021-04-29 14:43:51 +0800

    LeetCode-2-无重复字符最长子串

---
+++ coding/src/test/java/com/roadmap/algorithm/LengthOfLongestSubstring3Test.java
@@ -0,0 +1,14 @@
+package com.roadmap.algorithm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LengthOfLongestSubstring3Test {
+
+    @Test
+    void lengthOfLongestSubstring() {
+        LengthOfLongestSubstring3 o = new LengthOfLongestSubstring3();
+        int aabbccaa = o.lengthOfLongestSubstring("abcbabc");
+    }
+}
