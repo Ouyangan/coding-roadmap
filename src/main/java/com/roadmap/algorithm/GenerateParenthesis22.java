ddf873b277cb7129b3e3f26ef4a02506d701ba7c
an.ouyang <an.ouyang@zuopeng.com>
2021-04-30 10:57:34 +0800

    LeetCode-22-括号生成(回溯)

---
+++ src/main/java/com/roadmap/algorithm/GenerateParenthesis22.java
@@ -0,0 +1,34 @@
+package com.roadmap.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParenthesis22 {
+    public List<String> generateParenthesis(int n) {
+        //回溯
+        List<String> result = new ArrayList<>();
+        backTrace(n, n, new StringBuilder(), result);
+        return result;
+    }
+
+    private void backTrace(int left, int right, StringBuilder builder, List<String> result) {
+        if (left == 0 && right == 0) {
+            result.add(builder.toString());
+            return;
+        }
+        // (()))(
+        if (left > right) {
+            return;
+        }
+        if (left > 0) {
+            builder.append("(");
+            backTrace(left - 1, right, builder, result);
+            builder.deleteCharAt(builder.length() - 1);
+        }
+        if (right > 0) {
+            builder.append(")");
+            backTrace(left, right - 1, builder, result);
+            builder.deleteCharAt(builder.length() - 1);
+        }
+    }
+}
