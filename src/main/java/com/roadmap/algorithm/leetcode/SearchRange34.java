5f902bc31f7710aa05cc761a609f1cab050a1421
an.ouyang <an.ouyang@zuopeng.com>
2021-05-06 17:59:07 +0800

    LeetCode-34-在排序数组中查找元素的第一个和最后一个位置(二分法)

---
+++ src/main/java/com/roadmap/algorithm/leetcode/SearchRange34.java
@@ -0,0 +1,45 @@
+package com.roadmap.algorithm.leetcode;
+
+public class SearchRange34 {
+
+    public int[] searchRange(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+        if (nums.length == 1) {
+            if (nums[0] == target) {
+                return new int[]{0, 0};
+            } else {
+                return new int[]{-1, -1};
+            }
+        }
+        int len = nums.length;
+        int left = 0;
+        int right = len - 1;
+        //这里主要注意left=right,比如[1, 4] 4
+        while (left <= right) {
+            //二分法
+            int mid = (right + left) / 2;
+            if (nums[mid] == target) {
+                System.out.println(mid);
+                //中心扩散
+                int start = mid;
+                int end = mid;
+                //找左边为位置,注意此时需要start>0防止0时越界
+                while (start > 0 && nums[start] == nums[start - 1]) {
+                    start--;
+                }
+                //找右边位置
+                while (end < len - 1 && nums[end] == nums[end + 1]) {
+                    end++;
+                }
+                return new int[]{start, end};
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
---
+++ src/test/java/com/roadmap/algorithm/leetcode/SearchRange34Test.java
@@ -0,0 +1,14 @@
+package com.roadmap.algorithm.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchRange34Test {
+
+    @Test
+    void searchRange() {
+        SearchRange34 range = new SearchRange34();
+        range.searchRange(new int[]{1, 4}, 4);
+    }
+}
