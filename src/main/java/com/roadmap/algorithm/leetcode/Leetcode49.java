505fbe15166de73ead876e1807dd8122a35769b8
an.ouyang <an.ouyang@zuopeng.com>
2021-05-10 16:17:37 +0800

    LeetCode-49-字母异位词分组(哈希)

---
+++ src/main/java/com/roadmap/algorithm/leetcode/Leetcode49.java
@@ -0,0 +1,46 @@
+package com.roadmap.algorithm.leetcode;
+
+import java.util.*;
+
+//给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+//
+// 示例:
+//
+// 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出:
+//[
+//  ["ate","eat","tea"],
+//  ["nat","tan"],
+//  ["bat"]
+//]
+//
+// 说明：
+//
+//
+// 所有输入均为小写字母。
+// 不考虑答案输出的顺序。
+//
+// Related Topics 哈希表 字符串
+// 👍 733 👎 0
+public class Leetcode49 {
+    /**
+     * 利用hashMap存储排序后的key,value存储目标字符
+     *
+     * @param strs
+     * @return
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String key = String.valueOf(arr);
+            if (!map.containsKey(key)) {
+                List<String> list = new ArrayList<String>();
+                map.put(key, list);
+            }
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
