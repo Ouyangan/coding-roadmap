6698b9161db81d99ef9a0ae779e88cf2f639ea0d
an.ouyang <an.ouyang@zuopeng.com>
2021-05-14 18:21:34 +0800

    LeetCode-75-颜色分类(双指针)

---
+++ src/main/java/com/roadmap/algorithm/leetcode/Leetcode75.java
@@ -0,0 +1,88 @@
+package com.roadmap.algorithm.leetcode;
+//给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+//
+// 此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+//
+//
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [2,0,2,1,1,0]
+//输出：[0,0,1,1,2,2]
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [2,0,1]
+//输出：[0,1,2]
+//
+//
+// 示例 3：
+//
+//
+//输入：nums = [0]
+//输出：[0]
+//
+//
+// 示例 4：
+//
+//
+//输入：nums = [1]
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// n == nums.length
+// 1 <= n <= 300
+// nums[i] 为 0、1 或 2
+//
+//
+//
+//
+// 进阶：
+//
+//
+// 你可以不使用代码库中的排序函数来解决这道题吗？
+// 你能想出一个仅使用常数空间的一趟扫描算法吗？
+//
+// Related Topics 排序 数组 双指针
+// 👍 883 👎 0
+public class Leetcode75 {
+    public void sortColors(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int curr = 0;
+        //定义左右两个指针
+        //当前指针
+        while (curr <= right) {
+            //交换到0区域
+            if (nums[curr] == 0) {
+                swap(nums, left, curr);
+                left++;
+                curr++;
+                //不交换,位置后移
+            } else if (nums[curr] == 1) {
+                curr++;
+            } else {
+                //将2交换到2区域,同时2区域指针前移
+                swap(nums, curr, right);
+                right--;
+            }
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
