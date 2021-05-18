93ce2b8ef92329a178caac776828a1b902bd5ae0
an.ouyang <an.ouyang@zuopeng.com>
2021-05-18 14:50:02 +0800

    LeetCode-101-对称二叉树(递归)

---
+++ src/main/java/com/roadmap/algorithm/leetcode/Leetcode101.java
@@ -0,0 +1,54 @@
+package com.roadmap.algorithm.leetcode;
+
+//给定一个二叉树，检查它是否是镜像对称的。
+//
+//
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+//
+//     1
+//   / \
+//  2   2
+// / \ / \
+//3  4 4  3
+//
+//
+//
+//
+// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+//
+//     1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+//
+//
+//
+//
+// 进阶：
+//
+// 你可以运用递归和迭代两种方法解决这个问题吗？
+// Related Topics 树 深度优先搜索 广度优先搜索
+// 👍 1381 👎 0
+public class Leetcode101 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return recur(root.left, root.right);
+    }
+
+    private boolean recur(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return recur(left.left, right.right) && recur(left.right, right.left);
+    }
+}
