8bdb808f00ea370eafaaa74fbf2bc71953ad4e47
an.ouyang <981017952@qq.com>
2021-06-09 19:25:42 +0800

    LeetCode-234-回文链表

---
+++ src/main/java/com/roadmap/algorithm/leetcode/Leetcode234.java
@@ -0,0 +1,62 @@
+package com.roadmap.algorithm.leetcode;
+//请判断一个链表是否为回文链表。
+//
+// 示例 1:
+//
+// 输入: 1->2
+//输出: false
+//
+// 示例 2:
+//
+// 输入: 1->2->2->1
+//输出: true
+//
+//
+// 进阶：
+//你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+// Related Topics 链表 双指针
+// 👍 1007 👎 0
+public class Leetcode234 {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = reverse(firstHalfEnd.next);
+
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (result && p2 != null) {
+            if (p1.val != p2.val) {
+                result = false;
+            }
+            p2 = p2.next;
+            p1 = p1.next;
+        }
+        firstHalfEnd.next = reverse(secondHalfStart);
+        return result;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    private ListNode endOfFirstHalf(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
